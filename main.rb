fde0c73e796bc8f8a2278982ded47b54b76e771a
Jonathan Sivahera <jsivahera@icloud.com>
2022-05-21 19:08:40 +0200

    Display main app menu

---
+++ main.rb
@@ -0,0 +1,8 @@
+require './app'
+
+def display_app
+  puts "\nPlease choose an option by enter a number:"
+  puts ['1 - List all books', '2 - List all people', '3 - Create a person',
+        '4 - Create a book', '5 - Create a rental', '6 - List all rentals for a given person id',
+        '7 - Exit']
+end
