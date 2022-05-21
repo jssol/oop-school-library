8947cf53d8533b53f2ff58949672fb35b5455bde
Jonathan Sivahera <jsivahera@icloud.com>
2022-05-21 03:48:58 +0200

    Fix linters

--- trimmer_decorator.rb
+++ trimmer_decorator.rb
@@ -1,6 +1,7 @@
 require_relative './decorator'
 
 class TrimmerDecorator < Decorator
-  if @nameable.correct_name.length > 10
-    @nameable.correct_name.strip.slice(0,10)
+  def correct_name
+    @nameable.correct_name.strip.slice(0, 10) if @nameable.correct_name.length > 10
+  end
 end
