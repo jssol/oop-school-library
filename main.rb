d13d30319ee1a595c535b8b8cb7cbc07584b3372
Jonathan Sivahera <jsivahera@icloud.com>
2022-05-21 19:14:47 +0200

    Add create_student method

--- main.rb
+++ main.rb
@@ -6,3 +6,18 @@ def display_app
         '4 - Create a book', '5 - Create a rental', '6 - List all rentals for a given person id',
         '7 - Exit']
 end
+
+def create_student(app)
+  print "\nAge: "
+  age = gets.chomp
+  print "\nName: "
+  name = gets.chomp
+  print "\nHas parent permission? [y/N]: "
+  has_permission = true
+  permission_value = gets.chomp
+  permission = permission_value.capitalize
+  has_permission = false if permission.include?('N')
+  print "\nClassroom: "
+  classroom = gets.chomp
+  app.add_student(classroom, age.to_i, name, has_permission)
+end
