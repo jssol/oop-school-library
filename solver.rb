96dc3dc7e935cf4fb8a5329e6138172f7ab161cb
Jonathan Sivahera <jsivahera@icloud.com>
2022-05-28 15:02:36 +0200

    Add solution to edge case

--- solver.rb
+++ solver.rb
@@ -1,6 +1,6 @@
 class Solver
   def factorial(num)
-    # return "Please use a positive number" if num < 0
+    return "Please use a positive number" if num < 0
     return 1 if num == 0
     return num * factorial(num-1)
   end
