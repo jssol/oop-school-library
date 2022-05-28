efbf6cd1d82ad7860dfef87fe29c40f62b4a2e81
Jonathan Sivahera <jsivahera@icloud.com>
2022-05-28 14:44:41 +0200

    Write unit tests for the factorial method

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -1,38 +1,3 @@
 require './book'
 require './student'
 
-describe Book do
-  before :each do
-    @book = Book.new('title', 'author')
-  end
-
-  context '#new' do
-    it 'should return a book obj' do
-      expect(@book).to be_instance_of Book
-    end
-  end
-
-  context '#title' do
-    it 'should return the right title' do
-      expect(@book.title).to eql 'title'
-    end
-  end
-
-  context '#author' do
-    it 'should return the required author' do
-      expect(@book.author).to eql 'author'
-    end
-  end
-
-  it 'should put rental to be empty' do
-    expect(@book.rentals).to eql []
-  end
-
-  it 'should add a rental' do
-    @student = Student.new('basic4', 16, 'pako', true)
-    expect(@book.rentals.length).to eql(0)
-
-    @book.rentals.push(@student)
-    expect(@book.rentals.length).to eql(1)
-  end
-end
