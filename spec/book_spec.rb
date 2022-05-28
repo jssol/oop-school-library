de695a04bad069b194ebf88e39d06a4e4bad74c8
unknown <okorojiebube2@gmail.com>
2022-05-28 07:42:56 -0600

    Add Fizzbuzz test

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -1,3 +1,2 @@
 require './book'
 require './student'
-
--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -1,13 +1,5 @@
 require_relative '../solver'
 
-# Create a method called factorial that takes one argument, an integer N, and returns the factorial for that number. The factorial is the multiplication of all integers from 1 to N and has the special case that the factorial of 0 is 1. This method only accepts 0 and positive integers, so if a negative integer is given it should raise an exception.
-# Create a method called reverse that takes one argument, a string word, and returns word reversed (e.g. if word is "hello" it returns "olleh").
-# Create a method called fizzbuzz that takes one argument, an integer N, and returns a string. The returned string is constructed following these rules:
-# When N is divisible by 3, return "fizz".
-# When N is divisible by 5, return "buzz".
-# When N is divisible by 3 and 5, return "fizzbuzz".
-# Any other case, return N as a string (e.g. say N is 7 then return "7").
-
 describe Solver do
   before :each do
     @solver = Solver.new
@@ -16,7 +8,7 @@ describe Solver do
   context 'testing the factorial method' do
     it 'should return "Please use a positive number" when N is inferior to 0' do
       value = @solver.factorial(-2)
-      expect(value).to match("Please use a positive number")
+      expect(value).to match('Please use a positive number')
     end
 
     it 'should return 1 when N equals 0' do
@@ -32,11 +24,26 @@ describe Solver do
 
   context 'testing the reverse method' do
     it 'when given string is empty' do
-    expect(@solver.reverse('')).to eql ''
+      expect(@solver.reverse('')).to eql ''
+    end
+
+    it 'should return "olleH" when the given word is "Hello"' do
+      expect(@solver.reverse('Hello')).to eql 'olleH'
+    end
   end
 
-  it 'should return "olleH" when the given word is "Hello"' do
-    expect(@solver.reverse('Hello')).to eql 'olleH'
-  end
+  context 'testing the fizzbuzz method' do
+    it 'When N is divisible by 3, return "fizz"' do
+      expect(@solver.fizzbuzz(3)).to eql 'fizz'
+    end
+    it 'When N is divisible by 5, return "buzz"' do
+      expect(@solver.fizzbuzz(5)).to eql 'buzz'
+    end
+    it 'When N is divisible by 3 and 5, return "fizzbuzz"' do
+      expect(@solver.fizzbuzz(15)).to eql 'fizzbuzz'
+    end
+    it 'Any other case, return N as a string' do
+      expect(@solver.fizzbuzz(7)).to eql '7'
+    end
   end
 end
